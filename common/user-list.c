5fb899079a718b70a5e213b53449431a8081a989
pan93412 <28441561+pan93412@users.noreply.github.com>
2018-12-11 00:01:00 +0800

    Revert "Fix comment"
    This reverts commit 7241fd81c0dfcbe354f2c9bc4ace04088de5d860.

--- common/user-list.c
+++ common/user-list.c
@@ -1385,7 +1385,7 @@ common_user_get_session (CommonUser *user)
 /**
  * common_user_set_session:
  * @user: A #CommonUser
- * @session: The user's new session
+ * @language: The user's new session
  *
  * Set the session for a user.
  **/
